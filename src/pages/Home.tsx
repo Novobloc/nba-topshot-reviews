5cade4f8cb764ef5bf138d104265f1c2b6f2ae3e
Sandeep Kumar <satyasandeep786@gmail.com>
2023-02-28 00:07:56 +0530

    feat: Change button text in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,21 +36,13 @@ export default function Example() {
             </h1>
             <div className="flex flex-col items-center">
               <button
-                // type="submit"
                 onClick={redirectToMarketPlace}
-                className="flex w-64 h-14 max-w-xs font-extralight py-4 px-4 justify-center bg-gradient-to-r rounded-md border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full">
-                Explore Moments on{" "}
-                <img className="ml-2" width={100} height={100} src="https://nbatopshot.com/static/img/top-shot-logo-horizontal-white.svg" />
+                className="flex  max-w-xs font-extralight py-4 px-2 justify-center bg-gradient-to-r rounded-md border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full">
+                Explore Moments
+                {/* <img className="ml-2" width={100} height={100} src="https://nbatopshot.com/static/img/top-shot-logo-horizontal-white.svg" /> */}
                 <ArrowRightIcon className="ml-2" width={20} />
               </button>
             </div>
-            {/* className="w-64 h-14 flex py-3 px-8 max-w-xs justify-center font-extralight bg-gradient-to-r rounded-md  border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full" */}
-
-            {/* <Link
-              to={"/market-place"}
-              className="w-64 h-14 font-normal rounded-md  border border-transparent  bg-gradient-to-r from-indigo-800 via-blue-800 to-indigo-800  uppercase py-4 px-4 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 focus:ring-offset-slate-500">
-              Rate Now ⭐
-            </Link> */}
           </div>
         </div>
       </section>
