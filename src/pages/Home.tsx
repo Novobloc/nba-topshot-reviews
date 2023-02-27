2cea05b73a8cd3c952988a1cff231ba059cc6237
Sandeep Kumar <satyasandeep786@gmail.com>
2023-02-27 23:37:23 +0530

    feat: change button ui in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Sphere, OrbitControls, MeshDistortMaterial } from "@react-three/drei";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { StarIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 
 function SphereComp() {
   // const texture = useTexture('/terrazo.png')
@@ -14,6 +15,12 @@ function SphereComp() {
 }
 
 export default function Example() {
+  const navigate = useNavigate();
+
+  const redirectToMarketPlace = async () => {
+    return navigate("/market-place");
+  };
+
   return (
     <div className="">
       <section className="h-auto  tails-selected-element  z-50">
@@ -27,11 +34,23 @@ export default function Example() {
               <img className="flex items-center justify-center  mr-1" src="https://developers.nbatopshot.com/img/light.svg" alt="" />
               Moment?{" "}
             </h1>
-            <Link
+            <div className="flex flex-col items-center">
+              <button
+                // type="submit"
+                onClick={redirectToMarketPlace}
+                className="flex w-64 h-14 max-w-xs font-extralight py-4 px-4 justify-center bg-gradient-to-r rounded-md border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full">
+                Explore Moments on{" "}
+                <img className="ml-2" width={100} height={100} src="https://nbatopshot.com/static/img/top-shot-logo-horizontal-white.svg" />
+                <ArrowRightIcon className="ml-2" width={20} />
+              </button>
+            </div>
+            {/* className="w-64 h-14 flex py-3 px-8 max-w-xs justify-center font-extralight bg-gradient-to-r rounded-md  border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full" */}
+
+            {/* <Link
               to={"/market-place"}
               className="w-64 h-14 font-normal rounded-md  border border-transparent  bg-gradient-to-r from-indigo-800 via-blue-800 to-indigo-800  uppercase py-4 px-4 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 focus:ring-offset-slate-500">
               Rate Now ⭐
-            </Link>
+            </Link> */}
           </div>
         </div>
       </section>
