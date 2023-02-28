ab4f2aa4ee47732ad69fb329f8e3d860f134920b
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2023-02-28 18:02:32 +0530

    style: Update styles in discover

--- src/components/MarketPlace/Card.tsx
+++ src/components/MarketPlace/Card.tsx
@@ -1,114 +1,70 @@
-/* eslint-disable react/prop-types */
-import React, { useState } from "react";
-// import ReactModal from "./ReactModal";
+import React, { useEffect, useState } from "react";
+import { StarIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
 
-const CreatorCard = (props: any) => {
-  const { data } = props;
-  const [show, setShow] = useState(false);
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const momentTier: any = {
+  MOMENT_TIER_NIL: "bg-slate-100",
+  MOMENT_TIER_COMMON: "bg-slate-100",
+  MOMENT_TIER_RARE: "bg-yellow-400",
+  MOMENT_TIER_LEGENDARY: "bg-emerald-600",
+  MOMENT_TIER_ULTIMATE: "bg-purple-500",
+  MOMENT_TIER_FANDOM: "bg-blue-500",
+  MOMENT_TIER_ANTHOLOGY: "bg-red-500"
+};
+
+export default function Card({ product }: any) {
   return (
-    <div className="w-full border border-gray-200 rounded-lg shadow-sm">
-      <div className="flex flex-col items-center justify-center p-10">
-        <img className="w-32 h-32 mb-6 rounded-full" src={data.photo} />
-        <h2 className="text-lg font-medium">{data.name || "Fred Clemens"}</h2>
-        <p className="font-medium text-blue-500">{data.country || "India"}</p>
-        <p className="text-gray-400">{data.description || "Fred Clemens"}</p>
+    <div key={product.id} className="group relative border border-gray-200 p-4 sm:p-6">
+      <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75">
+        <img
+          src={product.assetPathPrefix + "Hero_2880_2880_Black.jpg?format=webp&quality=80&width=583&cv=1"}
+          alt={product.imageAlt}
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-      <div>
-        {true &&
-          true &&
-          data.tags.map((tag: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined, i: React.Key | null | undefined) => {
-            return (
-              <span className="px-2 py-1 m-1 text-green-800 text-xs font-medium bg-green-100 rounded-full" key={i}>
-                {tag}
-              </span>
-            );
-          })}
-      </div>
-      <br />
+      <div className="pt-10 pb-4 text-center">
+        <h3 className="text-sm font-medium text-gray-900">
+          <Link to={`/discover/view/${product.set.id}+${product.play.id}`}>
+            <span aria-hidden="true" className="absolute inset-0" />
+            <h2 className="text-lg font-mono font-bold"> {product.play.headline}</h2>
+            <p className="font-medium italic text-blue-800">{product.play.stats.teamAtMoment}</p>
+          </Link>
+        </h3>
+        <div className="mt-3 flex flex-col items-center">
+          <p className="sr-only">{product.reviews.average} out of 5 stars</p>
+          <div className="flex items-center">
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <StarIcon
+                key={rating}
+                className={classNames(product.reviews.average > rating ? "text-yellow-400" : "text-gray-200", "flex-shrink-0 h-5 w-5")}
+                aria-hidden="true"
+              />
+            ))}
+          </div>
+          <p className="mt-1 text-sm text-gray-500">{product.reviews.totalCount} reviews</p>
+        </div>
 
-      <div className="flex">
-        <a
-          href="#_"
-          className="flex-1 inline-flex block p-5 text-center text-gray-500 transition duration-200 ease-out hover:bg-gray-100 hover:text-gray-500">
-          <p>View</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6  mx-auto fill-current"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-          </svg>
-        </a>
-
-        <span
-          // href="#_"
-          onClick={() => setShow(true)}
-          className="flex-1 inline-flex block p-5 text-center text-gray-500 transition duration-200 ease-out hover:bg-gray-100 hover:text-gray-500">
-          <p>Donate</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6 mx-auto fill-current"
-            version="1.1"
-            shapeRendering="geometricPrecision"
-            textRendering="geometricPrecision"
-            imageRendering="optimizeQuality"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            viewBox="0 0 784.37 1277.39">
-            <g id="Layer_x0020_1">
-              <metadata id="CorelCorpID_0Corel-Layer" />
-              <g id="_1421394342400">
-                <g>
-                  <polygon fill="#343434" fillRule="nonzero" points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54 " />
-                  <polygon fill="#8C8C8C" fillRule="nonzero" points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33 " />
-                  <polygon fill="#3C3C3B" fillRule="nonzero" points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89 " />
-                  <polygon fill="#8C8C8C" fillRule="nonzero" points="392.07,1277.38 392.07,956.52 -0,724.89 " />
-                  <polygon fill="#141414" fillRule="nonzero" points="392.07,882.29 784.13,650.54 392.07,472.33 " />
-                  <polygon fill="#393939" fillRule="nonzero" points="0,650.54 392.07,882.29 392.07,472.33 " />
-                </g>
-              </g>
-            </g>
-          </svg>
+        <span className={classNames(momentTier[product.tier], "px-2 py-1 m-1 text-slate-800 text-xs font-medium rounded")}>
+          {product.tier.replace("MOMENT_TIER_", "")}
         </span>
-      </div>
-
-      <div className="flex border-t border-gray-200 divide-x divide-gray-200">
-        <a
-          href={data.instagram}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex-1 block p-5 text-center text-gray-300 transition duration-200 ease-out hover:bg-gray-100 hover:text-gray-500">
-          <svg className="w-6 h-6 mx-auto fill-current" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path
-              fillRule="evenodd"
-              d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
-        <a
-          href={data.twitter}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex-1 block p-5 text-center text-gray-300 transition duration-200 ease-out hover:bg-gray-100 hover:text-gray-500">
-          <svg className="w-6 h-6 mx-auto fill-current" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-          </svg>
-        </a>
-        <a
-          href={data.linkedIn}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex-1 block p-5 text-center text-gray-300 transition duration-200 ease-out hover:bg-gray-100 hover:text-gray-500">
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto fill-current" viewBox="0 0 24 24">
-            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-          </svg>
-        </a>
+        <p className="mt-2 text-base font-medium text-gray-900">{product.setPlay.circulations.circulationCount} CC</p>
+        <form className="mt-6">
+          <div className="sm:flex-col1 mt-10 flex">
+            <button
+              // type="submit"
+              // onClick={redirectToWebsite}
+              className="flex py-3 px-8 max-w-xs justify-center font-extralight bg-gradient-to-r rounded-md  border border-transparent bg-gray-900 text-base text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-slate-500 sm:w-full">
+              Buy on <img className="ml-2" width={100} height={100} src="https://nbatopshot.com/static/img/top-shot-logo-horizontal-white.svg" />
+              {/* <ArrowTopRightOnSquareIcon className="ml-2" width={25} /> */}
+            </button>
+          </div>
+        </form>
+        <p className="mt-2 text-sm italic font-thin  text-gray-900">{product.setPlay.circulations.burned} Moments burned</p>
       </div>
     </div>
   );
-};
-
-export default CreatorCard;
+}
